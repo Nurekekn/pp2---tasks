1059dd5b7bfaa28a1b02fd9182327928ea295ccc
Nurekekn <kuannuraly2008@gmail.com>
2026-02-06 16:30:17 +0600

    add 1 lab2

---
+++ practice1/python-basics/casting.py
@@ -0,0 +1,21 @@
+#1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+#2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+#3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+#4
+x = int(4) 
+y = int(76)
+z = int(57)
+#5
+x = float(56)
+y = float(45)
+z = float(47)
---
+++ practice1/python-basics/data_types.py
@@ -0,0 +1,16 @@
+x = 5
+print(type(x))
+#2
+y = 3.14
+print(type(y))
+#3
+
+name = "Alice"
+print(type(name))
+#4
+is_active = True
+print(type(is_active))
+
+#5
+numbers = [1, 2, 3]
+print(type(numbers))
